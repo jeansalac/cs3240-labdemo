5795ee64cab2991503f12cad1ea50b5eceb7ced4
JSalac <jms2wz@virginia.edu>
2016-02-22 18:02:06 -0500

    New add.py file

---
+++ add.py
@@ -0,0 +1,7 @@
+def main():
+    num1 = input("What's the first string you want to add?")
+    num2 = input("What's the second number you want to add?")
+    print(int(num1) + int(num2))
+
+if __name__ == "__main__":
+    main()
